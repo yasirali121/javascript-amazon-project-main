2d469ece545a7eb502792503b1a59cb0e8c1e485
yasirali <yasirali192233@gmail.com>
2024-07-19 12:17:30 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ products.forEach((product) =>{
 
 
   cartSummaryHTML += `
-   <div class="cart-item-container">
+   <div class="cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -106,6 +107,11 @@ document.querySelector('.js-order-summary')
     link.addEventListener('click', () =>{
       const productId = link.dataset.productId; 
        removeFromCart(productId);
+
+      const container =  document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
        
     });
   });
