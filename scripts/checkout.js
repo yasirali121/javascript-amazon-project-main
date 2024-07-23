87081a8924836e6acc5030bfbf7c3f7b4e0d0036
yasirali <yasirali192233@gmail.com>
2024-07-23 13:56:14 +0530

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart,removeFromCart } from "../data/cart.js";
+import { cart,removeFromCart, updateDeliveryOption} from "../data/cart.js";
 import { products } from "../data/products.js";
 import  formatCurrency  from "./utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -112,7 +112,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
     cartItem.deliveryOptionId;    
 
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
                   <input type="radio"
                   ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -148,3 +150,11 @@ document.querySelector('.js-order-summary')
        
     });
   });
+
+  document.querySelectorAll('.js-delivery-option')
+   .forEach((element) =>{
+     element.addEventListener('click',() =>{
+      const {productId,deliveryOptionId} = element.dataset;
+       updateDeliveryOption(productId,deliveryOptionId);
+     });
+   });
