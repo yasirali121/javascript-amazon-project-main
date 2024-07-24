56e0fd2e55f187ff125ac80074bbd37bdf710de1
yasirali <yasirali192233@gmail.com>
2024-07-24 14:07:15 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import  formatCurrency  from "../utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
- const deliveryDate = today.add('7','days');
-console.log( deliveryDate.format('dddd, MMMM D'));
+import {renderPaymentSummary} from './paymentSummary.js';
 
  export function renderOrderSummary(){
 
@@ -139,7 +134,8 @@ document.querySelector('.js-order-summary')
         `.js-cart-item-container-${productId}`
       );
       container.remove();
-       
+
+      renderPaymentSummary();  
     });
   });
 
@@ -149,6 +145,7 @@ document.querySelector('.js-order-summary')
       const {productId,deliveryOptionId} = element.dataset;
        updateDeliveryOption(productId,deliveryOptionId);
        renderOrderSummary();
+       renderPaymentSummary();
      });
    });
   }
