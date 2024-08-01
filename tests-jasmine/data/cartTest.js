a6f442b1b552c831bb3605732c9f7ff7797df9d1
yasirali <yasirali192233@gmail.com>
2024-08-01 17:23:14 +0530

    create automated tests

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -7,9 +7,9 @@ describe('test suite: add to cart', () =>{
     spyOn(localStorage, 'getItem').and.callFake(() =>{
       return JSON.stringify([{
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1,
+        quantity:1,
         deliveryOptionId: '1'
-      }]);
+    }]);
     }); 
     
     loadFromStorage();
@@ -19,7 +19,7 @@ describe('test suite: add to cart', () =>{
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].quantity).toEqual(2);
   });
 
   it('adds a new product to the cart', () =>{
@@ -35,6 +35,6 @@ describe('test suite: add to cart', () =>{
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(2);
+    expect(cart[0].quantity).toEqual(1);
   });
 });
