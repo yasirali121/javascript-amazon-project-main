4b4ab319ba75bf67d9a3409e4e283955d4fcad02
yasirali <yasirali192233@gmail.com>
2024-08-07 14:32:14 +0530

    convert loadproducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -64,6 +64,30 @@ console.log(date.toLocaleTimeString());*/
 
 export let products = [];
 
+ export function loadProductsFetch(){
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) =>{
+   return response.json()
+  }).then((productsData) =>{
+    products =productsData.map((productDetails) =>{
+      if (productDetails.type == 'clothing') {
+        return new clothing(productDetails);
+      }
+     return new product(productDetails);
+    });
+  
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() =>{
+  console.log('next step');
+});*/
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
