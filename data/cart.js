f8b205c6f9b7deb7a63db458f124a738c2fd5ea8
yasirali <yasirali192233@gmail.com>
2024-08-07 14:15:05 +0530

    learn promises and convert ocde to promises

--- data/cart.js
+++ data/cart.js
@@ -72,3 +72,16 @@ saveToStorage();
 
   saveToStorage();
 }
+
+export function loadCart(fun){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load',() =>{
+   console.log(xhr.response);
+
+  fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
